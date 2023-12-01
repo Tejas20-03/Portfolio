172e7c3d15bdf63be1365b40e80f98828870f567
Tejas20-03 <tejaschhabra.official@gmail.com>
2023-12-01 21:51:43 +0530

    Minor Changes

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ProjectCard from "../sub/ProjectCard";
+import Link from "next/link";
 
 const Projects = () => {
   return (
@@ -11,20 +12,22 @@ const Projects = () => {
         My Projects
       </h1>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
+
         <ProjectCard
-          src="/NextWebsite.png"
-          title="Modern Next.js Portfolio"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          src="/proj1.jpeg"
+          title="Smifty: Smart Wheelchair"
+          description="We have made a smart wheelchair prototype which uses YOLO v6 model to detect and avoid objects, will act as an aid for physically disabled people"
         />
         <ProjectCard
-          src="/CardImage.png"
-          title="Interactive Website Cards"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          src="/proj2.png"
+          title="Gbpiet-TnP_Portal"
+          description="A college training and placement website is a website that provides information and resources to students and alumni who are looking for jobs. The website may include information on upcoming job fairs, company profiles, and job postings. It may also offer resources such as resume writing tips, interview preparation tips, and salary negotiation tips.
+          The website may also have a section for employers who are looking to recruit students and alumni. This section may include information on how to post job openings, how to contact students and alumni, and how to schedule interviews."
         />
         <ProjectCard
-          src="/SpaceWebsite.png"
-          title="Space Themed Website"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          src="/proj3.png"
+          title="Spardha: Coding Competition Platform"
+          description="Spardha offers hundreds of competitive programming challenges. It is one of the best site for competitive programming that allows you to write code in their online editor and view a collection of challenges. The coding problems are separated into different categories based on your skill level. Competitive coding is a great way to improve your programming skills and learn new algorithms."
         />
       </div>
     </div>
--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import Link from "next/link"
 
 interface Props {
   src: string;
@@ -9,19 +10,20 @@ interface Props {
 
 const ProjectCard = ({ src, title, description }: Props) => {
   return (
-    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
-      <Image
-        src={src}
-        alt={title}
-        width={1000}
-        height={1000}
-        className="w-full object-contain"
-      />
+    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] content-cover">
+        <Image
+          src={src}
+          alt={title}
+          width={1000}
+          height={1000}
+          className="w-full object-contain"
+        />
 
-      <div className="relative p-4">
-        <h1 className="text-2xl font-semibold text-white">{title}</h1>
-        <p className="mt-2 text-gray-300">{description}</p>
-      </div>
+
+        <div className="relative p-4">
+          <h1 className="text-2xl font-semibold text-white">{title}</h1>
+          <p className="mt-2 text-gray-300">{description}</p>
+        </div>
     </div>
   );
 };
